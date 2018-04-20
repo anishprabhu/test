2145dde5c170fa8c79dca3a2e19228f8f23beddf
anishprabhu <anishprabhu7@gmail.com>
2018-04-20 17:39:50 +0530

    sidebar enhancements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+
 $(document).ready(function(){
+	/*function for Side Navbar Active*/
 	$(".sidenav a").on("click", function(){
 	   $(".sidenav").find(".active").removeClass("active");
 	   $(this).addClass("active");
 	});
+	//$("#SideNavBar").hide();
+	$(".navbar-toggle").click(function(){
+	    $("#SideNavBar").toggle();
+	});
+
 });
